d8fc9ddd0c1b8570a21603c9c7289c7c2d03f7db
TJ Salgado <tsalgado96@gmail.com>
2021-11-25 05:30:30 +0700

    Taking a different approach to calculator logic

--- src/App.js
+++ src/App.js
@@ -3,25 +3,114 @@ import { calcElem } from './utilities/CalculatorElements';
 import Button from './components/Button';
 import Display from './components/Display';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      mainDisplay: [0],
+      upperDisplay: []
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.clearAll = this.clearAll.bind(this);
+    this.pushNumber = this.pushNumber.bind(this);
+    this.pushMainDisplay = this.pushMainDisplay.bind(this);
+    this.pushFirstNumberMain = this.pushFirstNumberMain.bind(this);
+    this.resetMainDisplay = this.resetMainDisplay.bind(this);
+    this.pushUpperDisplay = this.pushUpperDisplay.bind(this);
+    this.resetUpperDisplay = this.resetUpperDisplay.bind(this);
   }
 
-  handleClick(event){
-    console.log(event.target.id)
+  handleClick(event) {
+    // eslint-disable-next-line default-case
+    switch (event.target.id) {
+      case 'clear':
+        this.clearAll();
+        break;
+      case 'zero':
+      case 'one':
+      case 'two':
+      case 'three':
+      case 'four':
+      case 'five':
+      case 'six':
+      case 'seven':
+      case 'eight':
+      case 'nine':
+        this.pushNumber(event.target.id);
+        break;
+    }
+  }
+
+  pushNumber(numString) {
+    let parsedNum = parseInt(document.getElementById(numString).textContent);
+
+    if (this.state.mainDisplay[0] === 0) {
+      this.pushFirstNumberMain(parsedNum);
+    } else {
+      this.pushMainDisplay(parsedNum);
+    }
+
+    if (this.state.upperDisplay[0] === 0) {
+      this.pushFirstNumberUpper(parsedNum);
+    } else {
+      this.pushUpperDisplay(parsedNum);
+    }
+  }
+
+  // ===========================
+  // MAIN DISPLAY
+  // ===========================
+  pushFirstNumberMain(num) {
+    this.setState({
+      mainDisplay: [num]
+    });
+  }
+
+  pushMainDisplay(num) {
+    this.setState({
+      mainDisplay: this.state.mainDisplay.concat(num)
+    });
+  }
+
+  resetMainDisplay() {
+    this.setState({
+      mainDisplay: [0]
+    });
+  }
+
+  // ===========================
+  // UPPER DISPLAY
+  // ===========================
+  pushFirstNumberUpper(num) {
+    this.setState({
+      upperDisplay: [num]
+    });
+  }
+
+  pushUpperDisplay(num) {
+    this.setState({
+      upperDisplay: this.state.upperDisplay.concat(num)
+    });
+  }
+
+  resetUpperDisplay() {
+    this.setState({
+      upperDisplay: []
+    });
+  }
+
+  clearAll() {
+    this.resetMainDisplay();
+    this.resetUpperDisplay();
   }
 
   render() {
     return (
       <div className="App">
         <div id="container">
-          <Display />
+          <Display mainDisplayValue={this.state.mainDisplay} upperDisplayValue={this.state.upperDisplay} />
           {calcElem.map((elem) => {
-            return <Button value={elem.value} id={elem.id} class={elem.class} handleClick={this.handleClick}/>;
+            return <Button value={elem.value} id={elem.id} class={elem.class} handleClick={this.handleClick} key={elem.id} />;
           })}
         </div>
       </div>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,8 @@
 function Display(props){
   return (
     <div id="display-container">
-      <div id="upper-display">UPPER</div>
-      <div id="display">DISPLAY</div>
+      <div id="upper-display">{props.upperDisplayValue}</div>
+      <div id="display">{props.mainDisplayValue}</div>
     </div>
   )
 }
